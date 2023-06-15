ae07df3fb635b711056ef04b975d2bdee5197a50
CHUPAPIMUNENE <evelinakarabaeva10@gmail.com>
2023-06-15 13:15:51 +0600

    finally finished

--- script.js
+++ script.js
@@ -9,32 +9,68 @@ async function getRate() {
   let response = await fetch(API + select.value);
   let rate = await response.json();
   console.log(rate);
-  let h3 = document.createElement("h3");
-  h3.innerHTML = `USD: ${rate.conversion_rates.USD}`;
-  info.appendChild(h3);
-  h3.style.marginTop = "80px";
-  let h31 = document.createElement("h3");
-  h31.innerHTML = `RUB: ${rate.conversion_rates.RUB}`;
-  info.appendChild(h31);
-  let h32 = document.createElement("h3");
-  h32.innerHTML = `EUR: ${rate.conversion_rates.EUR}`;
-  info.appendChild(h32);
-  let h33 = document.createElement("h3");
-  h33.innerHTML = `KGZ: ${rate.conversion_rates.KGS}`;
-  info.appendChild(h33);
-  let h34 = document.createElement("h3");
-  h34.innerHTML = `KZT: ${rate.conversion_rates.KZT}`;
-  info.appendChild(h34);
-  let h35 = document.createElement("h3");
-  h35.innerHTML = `CNY: ${rate.conversion_rates.CNY}`;
-  info.appendChild(h35);
+
+  let h3 = info.querySelector("h3");
+  if (h3) {
+    h3.innerHTML = `USD: ${rate.conversion_rates.USD}`;
+  } else {
+    h3 = document.createElement("h3");
+    h3.innerHTML = `USD: ${rate.conversion_rates.USD}`;
+    info.appendChild(h3);
+    h3.style.marginTop = "80px";
+  }
+
+  let h31 = info.querySelector("h3:nth-child(2)");
+  if (h31) {
+    h31.innerHTML = `RUB: ${rate.conversion_rates.RUB}`;
+  } else {
+    h31 = document.createElement("h3");
+    h31.innerHTML = `RUB: ${rate.conversion_rates.RUB}`;
+    info.appendChild(h31);
+  }
+
+  let h32 = info.querySelector("h3:nth-child(3)");
+  if (h32) {
+    h32.innerHTML = `EUR: ${rate.conversion_rates.EUR}`;
+  } else {
+    h32 = document.createElement("h3");
+    h32.innerHTML = `EUR: ${rate.conversion_rates.EUR}`;
+    info.appendChild(h32);
+  }
+
+  let h33 = info.querySelector("h3:nth-child(4)");
+  if (h33) {
+    h33.innerHTML = `KGZ: ${rate.conversion_rates.KGS}`;
+  } else {
+    h33 = document.createElement("h3");
+    h33.innerHTML = `KGZ: ${rate.conversion_rates.KGS}`;
+    info.appendChild(h33);
+  }
+
+  let h34 = info.querySelector("h3:nth-child(5)");
+  if (h34) {
+    h34.innerHTML = `KZT: ${rate.conversion_rates.KZT}`;
+  } else {
+    h34 = document.createElement("h3");
+    h34.innerHTML = `KZT: ${rate.conversion_rates.KZT}`;
+    info.appendChild(h34);
+  }
+
+  let h35 = info.querySelector("h3:nth-child(6)");
+  if (h35) {
+    h35.innerHTML = `CNY: ${rate.conversion_rates.CNY}`;
+  } else {
+    h35 = document.createElement("h3");
+    h35.innerHTML = `CNY: ${rate.conversion_rates.CNY}`;
+    info.appendChild(h35);
+  }
 }
 
 let select1 = document.getElementById("currency1");
 let select2 = document.getElementById("currency2");
 let convertButton = document.getElementById("counted");
 convertButton.addEventListener("click", countRate);
-let infic = document.getElementById("infik");
+let infik = document.getElementById("infik");
 
 async function countRate() {
   let response = await fetch(API + select1.value);
@@ -46,9 +82,14 @@ async function countRate() {
   let input1 = input.value;
   let convertedAmount = input1 * rate1 * rate2;
 
-  let h2 = document.createElement("h2");
-  h2.textContent = convertedAmount;
-  infik.appendChild(h2);
+  let h2 = infik.querySelector("h2");
+  if (h2) {
+    h2.textContent = convertedAmount;
+  } else {
+    h2 = document.createElement("h2");
+    h2.textContent = convertedAmount;
+    infik.appendChild(h2);
+  }
 
   input.value = "";
 }
